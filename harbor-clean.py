289b20cb462e673d3d8591d93f06ed0154767531
dawson2016 <dawson_2014@163.com>
2019-04-23 17:15:45 +0800

    Create harbor-clean.py

---
+++ harbor-clean.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+#coding:utf-8
+import requests
+url='https://xxx/api/repositories?project_id=5'
+durl='https://xxx/api/repositories/'
+postfix='/tags?detail=1'
+response  = requests.get(url)
+global null
+null = ''
+for i in eval(response.content):
+	if i['tags_count']>3:
+		response1  = requests.get(durl+i['name']+postfix)
+		res1=response1.content
+		resdata=[]
+		for j in eval(res1):
+			resdata.append(j['name'])
+		resdata.sort()
+		resdata.reverse()
+		for k in resdata[3:]:
+			print durl+i['name']+'/tags/'+str(k)
+      print '写你删除逻辑即可'
